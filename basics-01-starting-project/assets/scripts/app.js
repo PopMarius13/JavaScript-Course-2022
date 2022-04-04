659720946d4cbb17febd0f0ca875eeb85bd3677c
PopMarius13 <popmariusionut13@gmail.com>
2022-04-04 20:21:15 +0300

    Data Type & Arrays & Objects

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,56 @@
 const defaultResult = 0
 let currentResult = defaultResult
+let logEntries = []
 
-function add() {
-  currentResult = currentResult + userInput.value
-  outputResult(currentResult, '')
+// Gets input from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value)
 }
 
-addBtn.addEventListener('click', add())
+// Generates and writes calculation log
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+  const calculationDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+  outputResult(currentResult, calculationDescription) // from vendor.js file
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput()
+  const initialResult = currentResult
+  currentResult += parseInt(enteredNumber)
+  createAndWriteLog('+', initialResult, enteredNumber)
+  const logEntry = {
+    operation: "ADD",
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  }
+  logEntries.push(logEntry)
+  console.log(logEntry.number)
+  console.log(logEntries)
+}
+
+function subtract(){
+  const enteredNumber = getUserNumberInput()
+  const initialResult = currentResult
+  currentResult -= parseInt(enteredNumber)
+  createAndWriteLog('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput()
+  const initialResult = currentResult
+  currentResult *= parseInt(enteredNumber)
+  createAndWriteLog('*', initialResult, enteredNumber)
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput()
+  const initialResult = currentResult
+  currentResult /= parseInt(enteredNumber)
+  createAndWriteLog('/', initialResult, enteredNumber)
+}
+
+addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
